19cdf9bd9667bec990314e2c4a77a448d7c246a3
Chamara Herath <cmb.info96@gmail.com>
2023-01-08 17:41:14 +0530

    Update settings.py

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.getenv("STATIC_ROOT")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "MyPortfolio/static"),)
 
 MEDIA_URL = "media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_ROOT = os.getenv("MEDIA_ROOT")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
