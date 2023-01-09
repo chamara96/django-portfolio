6a03072ccc7c81cb21b19ea58ea98a96ebd940fe
Chamara Herath <cmb.info96@gmail.com>
2023-01-09 22:17:07 +0530

    Add site visit chart

--- resume/context_processors.py
+++ resume/context_processors.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from django.utils import timezone
+from django.db.models.functions import TruncDay
+from django.db.models import Count
 from .models import Contact, Log
 
 
@@ -8,8 +10,22 @@ def dashboard(request):
     logs = Log.objects.all()
     today = datetime.today().date()
 
+    log_chart = logs.annotate(
+        date=TruncDay('login_at')
+    ).values("date").annotate(
+        count=Count('id')
+    ).order_by("-date")
+
+    log_data = []
+    for l in log_chart:
+        log_data.append({
+            "date": l["date"].strftime("%Y-%m-%d"),
+            "count": l["count"]
+        })
+
     return {
         "contacts_count": contacts.count(),
+        "log_chart": log_data,
         "total_log_count": logs.count(),
         "today_log_count": logs.filter(login_at__date=today).count(),
         "last_log_time": logs.latest('login_at').login_at,
